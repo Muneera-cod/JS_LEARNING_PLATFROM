d153098500ae4eb23115de2ce45970a3bac597c1
Muneera <muneerakottarath99@gmail.com>
2025-01-17 19:13:30 +0530

    slightly changed

--- src/features/Admin/layout/AdminLayout.jsx
+++ src/features/Admin/layout/AdminLayout.jsx
@@ -3,13 +3,14 @@ import Sidebar from '../../../Components/ui/Sidebar'
 import { sideBarData } from '../Components/ui/Data'
 import Navbar from '../../../Components/ui/Navbar'
 function AdminLayout({children}) {
-  const view=useSelector((state)=>state.view.curView)
+  const view = useSelector((state)=>state.view.curView)
+  const subview = useSelector((state)=>state.view.curSubView)
   return (
      <>
          {view===0 && <Navbar />}
         <main className='min-h-screen bg-mainClr dark:bg-darkmodeMainClr  flex sm:flex-col  md:flex-row'>
-        <Sidebar Data={sideBarData}/>
-        <section className='w-full  sm:mb-[72px] md:mb-[0] md:ml-[74px] sm:p-[16px] md:p-[20px] text-lightmodeTextClr dark:text-darkmodeTextClr'>
+        { subview === 0 && <Sidebar Data={sideBarData}/>}
+        <section className={`w-full   text-lightmodeTextClr dark:text-darkmodeTextClr sm:p-[16px] md:p-[20px] ${ subview === 0 ? 'sm:mb-[72px] md:mb-[0] md:ml-[74px] ' : ''}`}>
            {children}
         </section>
            
