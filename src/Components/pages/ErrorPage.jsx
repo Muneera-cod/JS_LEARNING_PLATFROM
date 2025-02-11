e11901fddcc937ccc94ae292c8c60947324f8e5b
Muneera <muneerakottarath99@gmail.com>
2025-02-11 17:52:10 +0530

    error page modiefied to navigate back and updated vite

--- src/Components/pages/ErrorPage.jsx
+++ src/Components/pages/ErrorPage.jsx
@@ -1,11 +1,16 @@
 import { IconError404 } from "@tabler/icons-react"
-
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
 function ErrorPage() {
+  const navigate = useNavigate()
+  useEffect(()=>{
+    navigate(-1)
+  },[])
   return (
     <div className='flex flex-col items-center justify-center h-screen text-2xl text-red-500'>
-        <h1>404</h1>
-        <p>Page Not Found</p>
         <IconError404/>
+        <p>Page Not Found</p>
+        <button className="underline font-[700] mx-4" onClick={()=>navigate(-1)}>Go back</button>
     </div>
     )
 }
--- src/Components/pages/LoadingPage.jsx
+++ src/Components/pages/LoadingPage.jsx
@@ -3,8 +3,7 @@ import { Loader } from '@mantine/core'
 function LoadingPage() {
   return (
     <div className='min-h-screen min-w-full font-[700] bg-mainClr dark:bg-darkmodeMainClr text-secondaryClr flex items-center justify-center'>
-      {/* <Loader color="blue" size={60}/> */}
-      Loading...
+      <Loader color="yellow" size={60}/>
       </div>
   )
 }
